5f7f196817c0a863047a5c0ed135f17b09e5f8c1
mattdean <matthewallandean@gmail.com>
2021-01-15 09:39:01 -0700

    Comment out destinationRepo for now

--- test/destinationRepo-test.js
+++ test/destinationRepo-test.js
@@ -1,33 +1,33 @@
-import chai from 'chai';
-const expect = chai.expect;
+// import chai from 'chai';
+// const expect = chai.expect;
 
-import Traveler from '../src/users/traveler'
+// import Traveler from '../src/users/traveler'
 
-describe('See if the tests are running', function() {
-  let destinationsData;
-  let destinations;
+// describe('See if the tests are running', function() {
+//   let destinationsData;
+//   let destinations;
   
 
-  beforeEach(function() {
-    destinationsData = {
-      "id": 1,
-      "name": "Ham Leadbeater",
-      "travelerType": "relaxer",
-    };
+//   beforeEach(function() {
+//     destinationsData = {
+//       "id": 1,
+//       "name": "Ham Leadbeater",
+//       "travelerType": "relaxer",
+//     };
 
-    destination = new Traveler(travelerData);
-  })
+//     destination = new Traveler(travelerData);
+//   })
 
 
-  it('should store traveler data', function() {
-    expect(traveler).to.be.an.instanceOf(Traveler);
-    expect(traveler).to.deep.equal({
-      "id": 1,
-      "name": "Ham Leadbeater",
-      "travelerType": "relaxer",
-    });
-  })
+//   it('should store traveler data', function() {
+//     expect(traveler).to.be.an.instanceOf(Traveler);
+//     expect(traveler).to.deep.equal({
+//       "id": 1,
+//       "name": "Ham Leadbeater",
+//       "travelerType": "relaxer",
+//     });
+//   })
 
   
 
-});
+// });
