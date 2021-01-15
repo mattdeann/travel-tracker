afda179ff1655e43f60475f3b99d2df435481420
mattdean <matthewallandean@gmail.com>
2021-01-15 11:10:35 -0700

    Amp up css unecessarily

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -5,19 +5,32 @@ export const main = document.querySelector("main");
 
 
 // Functions
+
+// USING TRAVELERTRIPS IN MULTIPLE FUNCTIONS, SHOULD TRAVELER TRIPS BE HERE OR IN TRAVELER??
+
 export const displayTravelerTrips = (traveler, allTrips, allDestinations) => {
   const travelerTrips = allTrips.filter(trip => trip.userID === traveler.id);
   travelerTrips.forEach(trip => {
     main.insertAdjacentHTML('beforeend', `
-      <article class="trip">
-        <p class="detail date">${trip.date}</p>
-        <p class="detail place-and-time">${trip.duration} days in ${findDestinationByID(trip.destinationID, allDestinations).destination}</p>
-        <img src="${findDestinationByID(trip.destinationID, allDestinations).image}" alt="findDestinationByID(trip.destinationID, allDestinations).alt" height="100px" width="100px">
-        <p class="detail travelers">Travelers: ${trip.travelers}</p>
-        <p class="detail status">Status: ${trip.status}</p>
+      <article class="trip" style="background-image: url(${findDestinationByID(trip.destinationID, allDestinations).image})">
+        <section class="trip-summary">
+          <p class="detail date">${trip.date}</p>
+          <p class="detail place-and-time">${trip.duration} days in ${findDestinationByID(trip.destinationID, allDestinations).destination}</p>
+          <p class="detail travelers">Travelers: ${trip.travelers}</p>
+          <p class="detail status">Status: ${trip.status}</p>
+        </section>
       </article>
     `);
   })
 }
 
+
 export const findDestinationByID = (id, destinationsData) => destinationsData.find(destination => destination.id === id)
+
+export const totalCostAllTrips = (traveler, allTrips, allDestinations) => {
+  const travelerTrips = allTrips.filter(trip => trip.userID === traveler.id);
+  travelerTrips.reduce(trip => {
+
+  })
+  
+}
